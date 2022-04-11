62b477bd5facf72c068f43572109e66ba7f87e0e
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-04-11 17:50:25 +0800

    update

--- src/main/java/com/yunqiic/dataapi/dao/CustomConfigMapper.java
+++ src/main/java/com/yunqiic/dataapi/dao/CustomConfigMapper.java
@@ -1,10 +1,12 @@
 package com.yunqiic.dataapi.dao;
 
+import com.yunqiic.dataapi.core.Mapper;
+import com.yunqiic.dataapi.db.domain.Config;
 import com.yunqiic.dataapi.model.dto.ConfigDataSearch;
 
 import java.util.List;
 import java.util.Map;
 
-public interface CustomConfigMapper {
+public interface CustomConfigMapper extends Mapper<Config> {
     List<Map> getListByKey(ConfigDataSearch configDataSearch);
 }

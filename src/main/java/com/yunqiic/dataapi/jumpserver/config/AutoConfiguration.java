436fa77c20807dd5354f681b2f3d1a591eadc59a
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-04-12 14:08:26 +0800

    1. add get loginUrl method

--- src/main/java/com/yunqiic/dataapi/jumpserver/config/AutoConfiguration.java
+++ src/main/java/com/yunqiic/dataapi/jumpserver/config/AutoConfiguration.java
@@ -2,13 +2,18 @@ package com.yunqiic.dataapi.jumpserver.config;
 
 import com.github.zhangchunsheng.authentication.service.AuthenticationService;
 import com.github.zhangchunsheng.authentication.service.impl.AuthenticationServiceImpl;
+import com.yunqiic.dataapi.db.service.BaseService;
 import me.zhangchunsheng.mangix.common.config.MangixConfig;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AutoConfiguration {
+    private final Log logger = LogFactory.getLog(AutoConfiguration.class);
+
     @Autowired
     private Properties properties;
 
@@ -25,6 +30,7 @@ public class AutoConfiguration {
         MangixConfig mangixConfig = new MangixConfig();
         mangixConfig.setPrivateToken(properties.getPrivateToken());
         mangixConfig.setBaseUrl(properties.getBaseUrl());
+        logger.info("privateToken: " + mangixConfig.getPrivateToken());
         return mangixConfig;
     }
 
